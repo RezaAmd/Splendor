6344b9b0b7f341d59e656d49d90fb60cd71402b3
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2025-10-20 00:45:26 +0330

    🌱 add game entity.

---
+++ src/Domain/Entities/Games/GameEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities.Games;
+
+public class GameEntity : BaseEntity
+{
+    public int RoundNumber { get; private set; } = 1;
+    public bool IsStarted { get; private set; } = false;
+    public bool IsFinished { get; private set; } = false;
+    public long CreatedOn { get; set; }
+    public Guid? WinnerId { get; private set; }
+}
